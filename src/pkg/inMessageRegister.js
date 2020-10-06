d4343a7cba205a4ce4769be831d6697d2881ba9d
JaewonLee_macbookpro <lee.jaewon@nts-corp.com>
2020-10-06 17:57:36 +0900

    minor codestyle fix

--- src/pkg/inMessageRegister.js
+++ src/pkg/inMessageRegister.js
@@ -1,3 +1,4 @@
+const { value } = require("../../assets/secret");
 /**
  * 외부에서 메세지를 받아 서버에 전달하는 역할을 하는 함수
  * @param entryServer {EntryServer}
@@ -31,7 +32,6 @@ const registerFunction = (entryServer) => {
           break;
         }
         case "secret": {
-          const { value } = require("../../assets/secret");
           process.send({
             key: "secret",
             value,
